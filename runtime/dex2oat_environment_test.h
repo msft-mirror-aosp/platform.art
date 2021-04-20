hopez13/art

Do not open dex files in CLC if we only need to get dexopt status
The verifying the class loader context when calling GetDexOptNeeded
we only need the dex locations and the checksums. Opening the full
dex files may lead to in memory extraction which is expensive and
unnecessary.

Add a special path in ClassLoaderContext::OpenDexFiles which will
extract the locations and the checksums from the apk instead of
actually opening the dex files.

We re-uses the same logic in OpenDexFiles in order to avoid
implementing the opening algorithm twice (which, given all the edge
cases is not trivial).

Bug: 169869944
Test: test-art-host
Change-Id: Ic327889677ce697cd60c5c688281515b932a2a76

runtime/dex2oat_environment_test.h
@@ -70,6 +70,7 @@ class Dex2oatEnvironmentTest : public CommonRuntimeTest {
 
     // Verify the environment is as we expect
     std::vector<uint32_t> checksums;
+    std::vector<std::string> dex_locations;
     std::string error_msg;
     ASSERT_TRUE(OS::FileExists(GetSystemImageFile().c_str()))
       << "Expected pre-compiled boot image to be at: " << GetSystemImageFile();
@@ -78,7 +79,8 @@ class Dex2oatEnvironmentTest : public CommonRuntimeTest {
     ASSERT_TRUE(OS::FileExists(GetResourceOnlySrc1().c_str()))
       << "Expected stripped dex file to be at: " << GetResourceOnlySrc1();
     ASSERT_FALSE(
-        dex_file_loader.GetMultiDexChecksums(GetResourceOnlySrc1().c_str(), &checksums, &error_msg))
+        dex_file_loader.GetMultiDexChecksums(
+            GetResourceOnlySrc1().c_str(), &checksums, &dex_locations, &error_msg))
       << "Expected stripped dex file to be stripped: " << GetResourceOnlySrc1();
     ASSERT_TRUE(OS::FileExists(GetDexSrc2().c_str()))
       << "Expected dex file to be at: " << GetDexSrc2();

msft-mirror-aosp/platform.art

runtime/dex2oat_environment_test.h
@@ -70,6 +70,7 @@ class Dex2oatEnvironmentTest : public CommonRuntimeTest {
 
     // Verify the environment is as we expect
     std::vector<uint32_t> checksums;
+    std::vector<std::string> dex_locations;
     std::string error_msg;
     ASSERT_TRUE(OS::FileExists(GetSystemImageFile().c_str()))
       << "Expected pre-compiled boot image to be at: " << GetSystemImageFile();
@@ -78,7 +79,8 @@ class Dex2oatEnvironmentTest : public CommonRuntimeTest {
     ASSERT_TRUE(OS::FileExists(GetStrippedDexSrc1().c_str()))
       << "Expected stripped dex file to be at: " << GetStrippedDexSrc1();
     ASSERT_FALSE(
-        dex_file_loader.GetMultiDexChecksums(GetStrippedDexSrc1().c_str(), &checksums, &error_msg))
+        dex_file_loader.GetMultiDexChecksums(
+            GetStrippedDexSrc1().c_str(), &checksums, &dex_locations, &error_msg))
       << "Expected stripped dex file to be stripped: " << GetStrippedDexSrc1();
     ASSERT_TRUE(OS::FileExists(GetDexSrc2().c_str()))
       << "Expected dex file to be at: " << GetDexSrc2();